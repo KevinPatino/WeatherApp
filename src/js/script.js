a08e63d3e3f53588fd9f457aa35a7331b0c0558b
Kevin Patino <kevinpatcastrejon95@gmail.com>
2024-08-02 20:36:27 -0700

    Radar fetch is working and console logging the information

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,23 @@ document.addEventListener("DOMContentLoaded", ()=>{
 })
 
 async function main(){
-    console.log('main thread running');
-    //organize your js file(s)
+    const query = 'Kuma'
+    const response = await fetch(
+        `https://api.radar.io/v1/search/autocomplete?query=${query}&layers=locality&limit=5`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: "prj_test_pk_98fa065480783b475c382ae5405dbadd1bce9491",
+          },
+        }
+      );
+      
+      if (response.ok) {
+        const data = await response.json();  
+         // do something with data
+         console.log(data);
+      } else {
+        // Handle the error
+        console.error('Fetch error:', response.status, response.statusText);
+      }
 }
