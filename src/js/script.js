d26e8a41885a515bbdaa17e564a516675457fc66
ivanmatey <ivanmatey5@gmail.com>
2024-08-02 20:36:28 -0600

    finished bare design for the page

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,27 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    main();
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const citySelector = document.getElementById("favCities");
+  const citySearchBar = document.getElementById("searchCity");
+  const cityName = document.getElementById("cityName");
+  const favIcon = document.getElementById("favIcon");
+  const temperature = document.getElementById("temperature");
+  const weatherInfo = document.getElementById("weatherInfo");
+  const day1 = document.getElementById("day1");
+  const day2 = document.getElementById("day2");
+  const day3 = document.getElementById("day3");
+  const day4 = document.getElementById("day4");
+  const day5 = document.getElementById("day5");
+  const hour1 = document.getElementById("hour1");
+  const hour2 = document.getElementById("hour2");
+  const hour3 = document.getElementById("hour3");
+  const hour4 = document.getElementById("hour4");
+  const hour5 = document.getElementById("hour5");
+  const hour6 = document.getElementById("hour6");
+  const hour7 = document.getElementById("hour7");
+  const hour8 = document.getElementById("hour8");
+  main();
+});
 
-async function main(){
-    console.log('main thread running');
-    //organize your js file(s)
+async function main() {
+  console.log("main thread running");
+  //organize your js file(s)
 }
