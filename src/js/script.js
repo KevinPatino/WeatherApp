6a3ec31fa861da15228580e36b8c6472b3163fc2
ivanmatey <ivanmatey5@gmail.com>
2024-08-08 10:55:27 -0600

    first design for hourly forecast

--- src/js/script.js
+++ src/js/script.js
@@ -24,42 +24,41 @@ document.addEventListener("DOMContentLoaded", () => {
   main();
 
   //Get the date *today and in five days
-  var today = new Date()
+  var today = new Date();
   var year = today.getFullYear();
   var month = today.getMonth() + 1;
   var day = today.getDate();
   if (month < 10) {
-    month = '0' + month;
+    month = "0" + month;
   }
   if (day < 10) {
-    day = '0' + day;
+    day = "0" + day;
   }
- today = `${year}-${month}-${day}`
+  today = `${year}-${month}-${day}`;
 
- var inFivedays = new Date()
- inFivedays.setDate(inFivedays.getDate()+5);
- var Fyear = inFivedays.getFullYear();
- var Fmonth = inFivedays.getMonth() + 1;
- var Fday = inFivedays.getDate();
- if (Fmonth < 10) {
-   Fmonth = '0' + Fmonth;
- }
- if (Fday < 10) {
-   Fday = '0' + Fday;
- }
- inFivedays = `${Fyear}-${Fmonth}-${Fday}`
+  var inFivedays = new Date();
+  inFivedays.setDate(inFivedays.getDate() + 5);
+  var Fyear = inFivedays.getFullYear();
+  var Fmonth = inFivedays.getMonth() + 1;
+  var Fday = inFivedays.getDate();
+  if (Fmonth < 10) {
+    Fmonth = "0" + Fmonth;
+  }
+  if (Fday < 10) {
+    Fday = "0" + Fday;
+  }
+  inFivedays = `${Fyear}-${Fmonth}-${Fday}`;
 
- console.log(today)
- console.log(inFivedays)
+  console.log(today);
+  console.log(inFivedays);
 
   const weatherConverter = (weathercode) => {
     const weatherSymbol = weathercode.map((value) => {
       //      console.log("weather", value)
       switch (value) {
-        case 0:
-          //clear sky - sunny
-          return "resources/sun.png";
+        case 0: //clear sky - sunny
         case 1: //mainly clear
+          return "resources/sun.png";
         case 2: //partly cloudy
         case 3: //Overcast
           return "resources/cloudy-day.png";
@@ -141,154 +140,178 @@ document.addEventListener("DOMContentLoaded", () => {
   `;
     document.getElementById("daysContainer").innerHTML = fivedaysHTML;
   };
-  const threehoursInfo = (info)=>{
+  const threehoursInfo = (info) => {
     const hourly = info.hourly;
     let weather = hourly.weather_code;
     weather = weatherConverter(weather);
 
-    console.log("threehours", weather)
+    console.log("threehours", weather);
 
     const threehoursHTML = `
       <div class="hour" id="hour1">
-        <div>0am
+        <div class = "oneHour">
+          <p class = "time">0 am</p>
           <img src="${weather[0]}" alt="weather symbol">
           <p>${hourly.temperature_2m[0]}℃</p>
           <p>${hourly.relative_humidity_2m[0]}%</p>
         </div>
-        <div>1am
+        <div class = "oneHour">
+          <p class = "time">1 am</p>
           <img src="${weather[1]}" alt="weather symbol">
           <p>${hourly.temperature_2m[1]}℃</p>
           <p>${hourly.relative_humidity_2m[1]}%</p>
         </div>
-        <div>2am
+        <div class = "oneHour">
+          <p class = "time">2 am</p>
           <img src="${weather[2]}" alt="weather symbol">
           <p>${hourly.temperature_2m[2]}℃</p>
           <p>${hourly.relative_humidity_2m[2]}%</p>
         </div>
       </div>
       <div class="hour" id="hour2">
-        <div>3am
+        <div class = "oneHour">
+          <p class = "time">3 am</p>
           <img src="${weather[3]}" alt="weather symbol">
           <p>${hourly.temperature_2m[3]}℃</p>
           <p>${hourly.relative_humidity_2m[3]}%</p>
         </div>
-        <div>4am
+        <div class = "oneHour">
+          <p class = "time">4 am</p>
           <img src="${weather[4]}" alt="weather symbol">
           <p>${hourly.temperature_2m[4]}℃</p>
           <p>${hourly.relative_humidity_2m[4]}%</p>
         </div>
-        <div>5am
+        <div class = "oneHour">
+          <p class = "time">5 am</p>
           <img src="${weather[5]}" alt="weather symbol">
           <p>${hourly.temperature_2m[5]}℃</p>
           <p>${hourly.relative_humidity_2m[5]}%</p>
         </div>
       </div>
       <div class="hour" id="hour3">
-        <div>6am
+        <div class = "oneHour">
+          <p class = "time">6 am</p>
           <img src="${weather[6]}" alt="weather symbol">
           <p>${hourly.temperature_2m[6]}℃</p>
           <p>${hourly.relative_humidity_2m[6]}%</p>
         </div>
-        <div>7am
+        <div class = "oneHour">
+          <p class = "time">7 am</p>
           <img src="${weather[7]}" alt="weather symbol">
           <p>${hourly.temperature_2m[7]}℃</p>
           <p>${hourly.relative_humidity_2m[7]}%</p>
         </div>
-        <div>8am
+        <div class = "oneHour">
+          <p class = "time">8 am</p>
           <img src="${weather[8]}" alt="weather symbol">
           <p>${hourly.temperature_2m[8]}℃</p>
           <p>${hourly.relative_humidity_2m[8]}%</p>
         </div>
       </div>
       <div class="hour" id="hour4">
-        <div>9am
+        <div class = "oneHour">
+          <p class = "time">9 am</p>
           <img src="${weather[9]}" alt="weather symbol">
           <p>${hourly.temperature_2m[9]}℃</p>
           <p>${hourly.relative_humidity_2m[9]}%</p>
         </div>
-        <div>10am
+        <div class = "oneHour">
+          <p class = "time">10 am</p>
           <img src="${weather[10]}" alt="weather symbol">
           <p>${hourly.temperature_2m[10]}℃</p>
           <p>${hourly.relative_humidity_2m[10]}%</p>
         </div>
-        <div>11am
+        <div class = "oneHour">
+          <p class = "time">11 am</p>
           <img src="${weather[11]}" alt="weather symbol">
           <p>${hourly.temperature_2m[11]}℃</p>
           <p>${hourly.relative_humidity_2m[11]}%</p>
         </div>
       </div>
       <div class="hour" id="hour5">
-        <div>12pm
+        <div class = "oneHour">
+          <p class = "time">12 pm</p>
           <img src="${weather[12]}" alt="weather symbol">
           <p>${hourly.temperature_2m[12]}℃</p>
           <p>${hourly.relative_humidity_2m[12]}%</p>
         </div>
-        <div>1pm
+        <div class = "oneHour">
+          <p class = "time">1 pm</p>
           <img src="${weather[13]}" alt="weather symbol">
           <p>${hourly.temperature_2m[13]}℃</p>
           <p>${hourly.relative_humidity_2m[13]}%</p>
         </div>
-        <div>2pm
+        <div class = "oneHour">
+          <p class = "time">2 pm</p>
           <img src="${weather[14]}" alt="weather symbol">
           <p>${hourly.temperature_2m[14]}℃</p>
           <p>${hourly.relative_humidity_2m[14]}%</p>
         </div>
       </div>
       <div class="hour" id="hour6">
-        <div>3pm
+        <div class = "oneHour">
+          <p class = "time">3 pm</p>
           <img src="${weather[15]}" alt="weather symbol">
           <p>${hourly.temperature_2m[15]}℃</p>
           <p>${hourly.relative_humidity_2m[15]}%</p>
         </div>
-        <div>4pm
+        <div class = "oneHour">
+          <p class = "time">4 pm</p>
           <img src="${weather[16]}" alt="weather symbol">
           <p>${hourly.temperature_2m[16]}℃</p>
           <p>${hourly.relative_humidity_2m[16]}%</p>
         </div>
-        <div>5pm
+        <div class = "oneHour">
+          <p class = "time">5 pm</p>
           <img src="${weather[17]}" alt="weather symbol">
           <p>${hourly.temperature_2m[17]}℃</p>
           <p>${hourly.relative_humidity_2m[17]}%</p>
         </div>
       </div>
       <div class="hour" id="hour7">
-        <div>6pm
+        <div class = "oneHour">
+          <p class = "time">6 pm</p>
           <img src="${weather[18]}" alt="weather symbol">
           <p>${hourly.temperature_2m[18]}℃</p>
           <p>${hourly.relative_humidity_2m[18]}%</p>
         </div>
-        <div>7pm
+        <div class = "oneHour">
+          <p class = "time">7 pm</p>
           <img src="${weather[19]}" alt="weather symbol">
           <p>${hourly.temperature_2m[19]}℃</p>
           <p>${hourly.relative_humidity_2m[19]}%</p>
         </div>
-        <div>8pm
+        <div class = "oneHour">
+          <p class = "time">8 pm</p>
           <img src="${weather[20]}" alt="weather symbol">
           <p>${hourly.temperature_2m[20]}℃</p>
           <p>${hourly.relative_humidity_2m[20]}%</p>
         </div>
       </div>
       <div class="hour" id="hour8">
-        <div>9pm
+        <div class = "oneHour">
+          <p class = "time">9 pm</p>
           <img src="${weather[21]}" alt="weather symbol">
           <p>${hourly.temperature_2m[21]}℃</p>
           <p>${hourly.relative_humidity_2m[21]}%</p>
         </div>
-        <div>10pm
+        <div class = "oneHour">
+          <p class = "time">10 pm</p>
           <img src="${weather[22]}" alt="weather symbol">
           <p>${hourly.temperature_2m[22]}℃</p>
           <p>${hourly.relative_humidity_2m[22]}%</p>
         </div>
-        <div>11pm
+        <div class = "oneHour">
+          <p class = "time">11 pm</p>
           <img src="${weather[23]}" alt="weather symbol">
           <p>${hourly.temperature_2m[23]}℃</p>
           <p>${hourly.relative_humidity_2m[23]}%</p>
         </div>
       </div>
-    `
+    `;
 
-    document.getElementById("hoursContainer").innerHTML = threehoursHTML
-  }
+    document.getElementById("hoursContainer").innerHTML = threehoursHTML;
+  };
   const fetch5daysWeather = async () => {
     try {
       const response = await fetch(
